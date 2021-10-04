03e45f4bf7bbfa8ad27f9cb6f246707e382f8478
Fernanda <fernandaarduini@hotmail.com>
2021-10-04 20:11:58 -0300

    Primeiro Commit

---
+++ Enum/Genero.cs
@@ -0,0 +1,19 @@
+namespace FER.Series
+{
+	public enum Genero
+	{
+		Teen = 1,
+		Aventura = 2,
+		Comedia = 3,
+		Herois = 4,
+		ETs = 5,
+		Princesas = 6,
+		Gatos = 7,
+		Marvel = 8,
+		DC = 9,
+		Mexicana = 10,
+		Romance = 11,
+		Suspense = 12,
+		Terror = 13
+	}
+}
